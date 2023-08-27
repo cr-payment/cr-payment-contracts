9d08b4b152962737efb8907a68b45305ccad47af
quThinh <thinhtruong@var-meta.com>
2023-08-27 18:13:51 +0700

    update test file

--- contracts/CrPayment.sol
+++ contracts/CrPayment.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.18;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
 contract CrPayment is Ownable {
     using Address for address payable;
@@ -13,10 +14,14 @@ contract CrPayment is Ownable {
 
     address public feeTo;
     uint256 public platformFeeBasisPoints;
+    address public verifier;
+
+    // mapping used signature
+    mapping(bytes => bool) private invalidSignatures;
 
     event FeeToAddressSet(address _feeTo);
     event PlatformFeeBasisPointsSet(uint256 _platformFeeBasisPoints);
-
+    event VerifierAddressChanged(address oldAddress, address newAddress);
     event Payment(
         uint256 indexed _sessionId,
         address indexed _merchant,
@@ -31,7 +36,11 @@ contract CrPayment is Ownable {
      * @notice The platform fee basis points must be less than 10000
      * @notice The fee to address must not be the zero address
      */
-    constructor(address _feeTo, uint256 _platformFeeBasisPoints) {
+    constructor(
+        address _feeTo,
+        uint256 _platformFeeBasisPoints,
+        address _verifier
+    ) {
         require(
             _platformFeeBasisPoints < BASIS_POINTS,
             "Invalid platform fee basis points"
@@ -39,6 +48,7 @@ contract CrPayment is Ownable {
         require(_feeTo != address(0), "Invalid fee to address");
 
         feeTo = _feeTo;
+        verifier = _verifier;
         platformFeeBasisPoints = _platformFeeBasisPoints;
     }
 
@@ -70,18 +80,37 @@ contract CrPayment is Ownable {
     }
 
     /**
-     * @dev Pay the merchant
-     * @param _sessionId The payment session id
-     * @param _merchant The merchant address
-     * @param _token The token address
-     * @param _amount The amount of tokens
+     * @dev Initiates a payment to a merchant with verified signature and details
+     * If the payment is valid, fees are deducted and the payment is processed.
+     *
+     * @param _sessionId The ID of the payment session.
+     * @param _merchant The address of the merchant receiving the payment.
+     * @param _token The address of the token used for payment.
+     * @param _amount The amount of tokens or Ether being paid.
+     * @param _deadline The expiration time for the provided signature.
+     * @param signature The signature provided by the payer.
      */
     function pay(
         uint256 _sessionId,
         address _merchant,
         address _token,
-        uint256 _amount
+        uint256 _amount,
+        uint256 _deadline,
+        bytes memory signature
     ) external payable {
+        require(
+            invalidSignatures[signature] == false &&
+                verifyPayment(
+                    _sessionId,
+                    _merchant,
+                    msg.sender,
+                    _token,
+                    _amount,
+                    _deadline,
+                    signature
+                ),
+            "Invalid signature"
+        );
         uint256 feeAmount = (_amount * platformFeeBasisPoints) / BASIS_POINTS;
         if (_token == address(0)) {
             require(msg.value == _amount, "Invalid price");
@@ -97,5 +126,95 @@ contract CrPayment is Ownable {
         }
 
         emit Payment(_sessionId, _merchant, _token, _amount);
+    }
+
+    /**
+     * @dev Verifies the validity of a payment signature.
+     *
+     * @param _sessionId The ID of the payment session.
+     * @param _merchant The address of the merchant receiving the payment.
+     * @param _sender The address of the customer.
+     * @param _token The address of the token used for payment.
+     * @param _amount The amount of tokens being paid.
+     * @param _deadline The expiration time for the provided signature.
+     * @param signature The signature provided by the payer.
+     * @return A boolean indicating whether the payment is valid or not.
+     */
+    function verifyPayment(
+        uint256 _sessionId,
+        address _merchant,
+        address _sender,
+        address _token,
+        uint256 _amount,
+        uint256 _deadline,
+        bytes memory signature
+    ) private view returns (bool) {
+        bytes32 dataHash = encodePayment(
+            _sessionId,
+            _merchant,
+            _sender,
+            _token,
+            _amount,
+            _deadline
+        );
+        bytes32 signHash = ECDSA.toEthSignedMessageHash(dataHash);
+        address recovered = ECDSA.recover(signHash, signature);
+        return recovered == verifier;
+    }
+
+    /**
+     * @dev Generates a unique hash representing payment details, including the session ID, merchant address,
+     * sender's address, token address, payment amount, and signature expiration time.
+     *
+     * @param _sessionId The ID of the payment session.
+     * @param _merchant The address of the merchant receiving the payment.
+     * @param _sender The address of the customer initiating the payment.
+     * @param _token The address of the token used for payment.
+     * @param _amount The amount of tokens being paid.
+     * @param _deadline The expiration time for the provided signature.
+     * @return A bytes32 hash representing the encoded payment details.
+     */
+    function encodePayment(
+        uint256 _sessionId,
+        address _merchant,
+        address _sender,
+        address _token,
+        uint256 _amount,
+        uint256 _deadline
+    ) private view returns (bytes32) {
+        uint256 id;
+        assembly {
+            id := chainid()
+        }
+        return
+            keccak256(
+                abi.encode(
+                    id,
+                    _sessionId,
+                    _merchant,
+                    _sender,
+                    _token,
+                    _amount,
+                    _deadline
+                )
+            );
+    }
+
+    /**
+     * @dev function to set verifier address
+     * @param _verifier new verifier address
+     */
+    function setVerifier(address _verifier) external onlyOwner {
+        address oldVerifier = verifier;
+        require(
+            _verifier != address(0),
+            "Set to zero address"
+        );
+        require(
+            _verifier != oldVerifier,
+            "verifier address set"
+        );
+        verifier = _verifier;
+        emit VerifierAddressChanged(oldVerifier, _verifier);
     }
 }
